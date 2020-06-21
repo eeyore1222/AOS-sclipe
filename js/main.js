01d4bccb4c75b59f8c801aa1cf83cbca32f1947a
eeyore1222 <eeyore951222@gmail.com>
2020-06-21 16:21:22 +0800

    footer&body

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-$('#banner').slick();
+AOS.init();
+
+$('#banner').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+$('#brand').slick({
+    dots: true,
+    slidesToShow: 5,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
